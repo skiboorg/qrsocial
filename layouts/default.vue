9d9ba2b846cae5afcb6e191755c4bf9088359b4f
xxx <11@11.11>
2020-10-27 18:07:18 +0300

    init

---
+++ layouts/default.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="">
+     <Header/>
+    <Nuxt />
+  <Footer/>
+  </div>
+
+
+</template>
+
+
+
+<script>
+    import Header from '@/components/header';
+    import Footer from '@/components/footer';
+    export default {
+        data:function(){
+            return{
+                isVisible:false,
+            }
+        },
+
+
+        components:{
+            Header,
+            Footer,
+
+        },
+        methods: {
+
+        },
+    }
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,395 @@
+<template>
+  <div>
+    <section class="banner" style="background: url(/banner.png) no-repeat center;height: 800px"></section>
+    <section class="features">
+      <div class="container">
+        <div class="features-big-wrapper">
+          <div class="features-text-item">
+            <p class="features-text-item__title">少儿英语信息技术（IT）编程课程</p>
+            <p class="features-text-item__text">编程是一个很大的领域，不仅包括数学和逻辑学，还包括处理信息和算法的能力 我们教授您的孩子从入门到高层次知识，期间考虑了所有学习的特殊性，使课堂变得有趣且具吸引力——并为此设计了一个阶段性学习系统。</p>
+          </div>
+          <div class="">
+            <img src="/feature-img-1.png" alt="">
+          </div>
+          <div class="">
+            <img src="/features-img-2.png" alt="">
+          </div>
+          <div class="features-text-item">
+            <p class="features-text-item__title">少儿英语信息技术（IT）编程课程</p>
+            <p class="features-text-item__text">编程是一个很大的领域，不仅包括数学和逻辑学，还包括处理信息和算法的能力 我们教授您的孩子从入门到高层次知识，期间考虑了所有学习的特殊性，使课堂变得有趣且具吸引力——并为此设计了一个阶段性学习系统。</p>
+          </div>
+          <div class="features-text-item">
+            <p class="features-text-item__title">少儿英语信息技术（IT）编程课程</p>
+            <p class="features-text-item__text">编程是一个很大的领域，不仅包括数学和逻辑学，还包括处理信息和算法的能力 我们教授您的孩子从入门到高层次知识，期间考虑了所有学习的特殊性，使课堂变得有趣且具吸引力——并为此设计了一个阶段性学习系统。</p>
+          </div>
+          <div class="">
+            <img src="/feature-img-1.png" alt="">
+          </div>
+          <div class="">
+            <img src="/features-img-2.png" alt="">
+          </div>
+          <div class="features-text-item">
+            <p class="features-text-item__title">少儿英语信息技术（IT）编程课程</p>
+            <p class="features-text-item__text">编程是一个很大的领域，不仅包括数学和逻辑学，还包括处理信息和算法的能力 我们教授您的孩子从入门到高层次知识，期间考虑了所有学习的特殊性，使课堂变得有趣且具吸引力——并为此设计了一个阶段性学习系统。</p>
+          </div>
+
+
+
+        </div>
+
+      </div>
+    </section>
+    <p class="section-header style-outline text-center mb-60">女孩名单</p>
+    <p class="section-subheader  text-center mb-60">女孩名单</p>
+    <section class="models">
+      <div class="container">
+        <div class="models-wrapper">
+
+          <div class="model-item">
+            <nuxt-link to="/girl-profile">
+              <div class="model-item__bg" style="background: url(/test_bg.png)"></div>
+              <div class="model-item__img">
+                <img src="/test_w.png" alt="">
+              </div>
+              <p class="model-item__name">Alice Krystal</p>
+              <p class="model-item__login">@kistochka</p>
+
+              <div class="model-item__info">
+                <div class="model-item__info-left">
+                  <p class="model-item__info-title">年龄</p>
+                  <p class="model-item__info-text">24岁</p>
+                </div>
+                <div class="model-item__info-right">
+                  <p class="model-item__info-title">兴趣爱好</p>
+                  <p class="model-item__info-text">滑雪，乐透...</p>
+                </div>
+              </div>
+              <div class="model-item__buttons">
+                <div class="btn btn-blue btn-round"><p> 写短信</p></div>
+                <div class="btn btn-outline btn-outline-gold ">
+                  <div class="btn-outline-inner">
+                    <p>添加</p>
+                  </div>
+                </div>
+              </div>
+            </nuxt-link>
+          </div>
+          <div class="model-item">
+            <nuxt-link to="/girl-profile">
+              <div class="model-item__bg" style="background: url(/test_bg.png)"></div>
+              <div class="model-item__img">
+                <img src="/test_w.png" alt="">
+              </div>
+              <p class="model-item__name">Alice Krystal</p>
+              <p class="model-item__login">@kistochka</p>
+
+              <div class="model-item__info">
+                <div class="model-item__info-left">
+                  <p class="model-item__info-title">年龄</p>
+                  <p class="model-item__info-text">24岁</p>
+                </div>
+                <div class="model-item__info-right">
+                  <p class="model-item__info-title">兴趣爱好</p>
+                  <p class="model-item__info-text">滑雪，乐透...</p>
+                </div>
+              </div>
+              <div class="model-item__buttons">
+                <div class="btn btn-blue btn-round"><p> 写短信</p></div>
+                <div class="btn btn-outline btn-outline-gold ">
+                  <div class="btn-outline-inner">
+                    <p>添加</p>
+                  </div>
+                </div>
+              </div>
+            </nuxt-link>
+          </div>
+          <div class="model-item">
+            <nuxt-link to="/girl-profile">
+              <div class="model-item__bg" style="background: url(/test_bg.png)"></div>
+              <div class="model-item__img">
+                <img src="/test_w.png" alt="">
+              </div>
+              <p class="model-item__name">Alice Krystal</p>
+              <p class="model-item__login">@kistochka</p>
+
+              <div class="model-item__info">
+                <div class="model-item__info-left">
+                  <p class="model-item__info-title">年龄</p>
+                  <p class="model-item__info-text">24岁</p>
+                </div>
+                <div class="model-item__info-right">
+                  <p class="model-item__info-title">兴趣爱好</p>
+                  <p class="model-item__info-text">滑雪，乐透...</p>
+                </div>
+              </div>
+              <div class="model-item__buttons">
+                <div class="btn btn-blue btn-round"><p> 写短信</p></div>
+                <div class="btn btn-outline btn-outline-gold ">
+                  <div class="btn-outline-inner">
+                    <p>添加</p>
+                  </div>
+                </div>
+              </div>
+            </nuxt-link>
+          </div>
+
+        </div>
+      </div>
+    </section><!--models-->
+    <p class="section-header style-outline text-center mb-60">女孩名单</p>
+    <section class="streams">
+      <div class="container">
+        <div class="streams-wrapper">
+          <div class="stream-item ">
+            <nuxt-link to="/stream">
+              <div class="stream-item__top" style="background: linear-gradient(black, black), url(/test.png) no-repeat center">
+                <div class="stream-item__top-btn">
+                  <div class="stream-item__top-btn--inner">
+                    <img draggable="false" src="/play_icon.png" alt="">
+                  </div>
+                </div>
+                <div class="stream-item__top-for-vip">
+                  <img draggable="false" src="/stream_for_vip_icon.png" alt="">
+                  <p>只限VIP</p>
+                </div>
+              </div>
+              <div class="stream-item__bottom">
+                <div class="stream-item__bottom--left">
+                  <p>玛丽的追求</p>
+                  <p>4小时前上传</p>
+                </div>
+                <div class="stream-item__bottom--right">
+                  <img draggable="false" src="http://placehold.it/300" alt="">
+                </div>
+              </div>
+            </nuxt-link>
+          </div>
+          <div class="stream-item ">
+            <nuxt-link to="/stream">
+              <div class="stream-item__top" style="background: linear-gradient(black, black), url(/test.png) no-repeat center">
+                <div class="stream-item__top-btn">
+                  <div class="stream-item__top-btn--inner">
+                    <img draggable="false" src="/play_icon.png" alt="">
+                  </div>
+                </div>
+                <div class="stream-item__top-for-vip">
+                  <img draggable="false" src="/stream_for_vip_icon.png" alt="">
+                  <p>只限VIP</p>
+                </div>
+              </div>
+              <div class="stream-item__bottom">
+                <div class="stream-item__bottom--left">
+                  <p>玛丽的追求</p>
+                  <p>4小时前上传</p>
+                </div>
+                <div class="stream-item__bottom--right">
+                  <img draggable="false" src="http://placehold.it/300" alt="">
+                </div>
+              </div>
+            </nuxt-link>
+          </div>
+          <div class="stream-item stream-vip">
+            <nuxt-link to="/stream">
+              <div class="stream-item__top" style="background: linear-gradient(black, black), url(/test.png) no-repeat center">
+                <div class="stream-item__top-btn">
+                  <div class="stream-item__top-btn--inner">
+                    <img draggable="false" src="/play_icon.png" alt="">
+                  </div>
+                </div>
+                <div class="stream-item__top-for-vip">
+                  <img draggable="false" src="/stream_for_vip_icon.png" alt="">
+                  <p>只限VIP</p>
+                </div>
+              </div>
+              <div class="stream-item__bottom">
+                <div class="stream-item__bottom--left">
+                  <p>玛丽的追求</p>
+                  <p>4小时前上传</p>
+                </div>
+                <div class="stream-item__bottom--right">
+                  <img draggable="false" src="http://placehold.it/300" alt="">
+                </div>
+              </div>
+            </nuxt-link>
+          </div>
+          <div class="stream-item stream-vip">
+            <nuxt-link to="/stream">
+              <div class="stream-item__top" style="background: linear-gradient(black, black), url(/test.png) no-repeat center">
+                <div class="stream-item__top-btn">
+                  <div class="stream-item__top-btn--inner">
+                    <img draggable="false" src="/play_icon.png" alt="">
+                  </div>
+                </div>
+                <div class="stream-item__top-for-vip">
+                  <img draggable="false" src="/stream_for_vip_icon.png" alt="">
+                  <p>只限VIP</p>
+                </div>
+              </div>
+              <div class="stream-item__bottom">
+                <div class="stream-item__bottom--left">
+                  <p>玛丽的追求</p>
+                  <p>4小时前上传</p>
+                </div>
+                <div class="stream-item__bottom--right">
+                  <img draggable="false" src="http://placehold.it/300" alt="">
+                </div>
+              </div>
+            </nuxt-link>
+          </div>
+        </div>
+      </div>
+    </section>
+    <p class="section-header style-outline text-center mb-60">女孩名单</p>
+
+    <div style="margin-bottom: 150px" class="container">
+      <div class="features-small-wrapper">
+        <div class="feature-small-item">
+          <img draggable="false" src="/feature-small-icon1.png" alt="">
+          <p>与来自俄罗斯各地<br>的女孩聊天24/7</p>
+        </div>
+        <div class="feature-small-item">
+          <img draggable="false" src="/feature-small-icon2.png" alt="">
+          <p>熟人和共同利益</p>
+        </div>
+        <div class="feature-small-item">
+          <img draggable="false" src="/feature-small-icon3.png" alt="">
+          <p>用你的闺蜜在线播放</p>
+        </div>
+        <div class="feature-small-item">
+          <img draggable="false" src="/feature-small-icon4.png" alt="">
+          <p>内置在线翻译员</p>
+        </div>
+
+      </div>
+    </div>
+    <p style="margin-bottom: 100px" class="section-header style-outline text-center ">女孩名单</p>
+    <section class="tariff">
+      <div class="container">
+        <div class="tariff-wrapper">
+          <div class="tariff-one">
+            <div class="tariff-one__wrapper">
+              <p class="tariff-one__name">VIP 1monts</p>
+              <div class="tariff-one__price">
+                <p>124.00Y</p>
+              </div>
+              <div class="tariff-one__info">
+                <p class="tariff-one__info-item">+一般聊天中的交流</p>
+                <p class="tariff-one__info-item">+使用相机</p>
+                <p class="tariff-one__info-item">+送礼物的能力</p>
+              </div>
+              <div class="btn btn-blue"><p>text</p></div>
+            </div>
+
+
+          </div>
+          <div class="tariff-two">
+            <div class="tariff-two__wrapper">
+              <p class="tariff-two__name">VIP 6 monts</p>
+              <div class="tariff-two__price">
+                <p>124.00Y</p>
+              </div>
+              <div class="tariff-two__info">
+                <p class="tariff-two__info-item">+一般聊天中的交流</p>
+                <p class="tariff-two__info-item">+使用相机</p>
+                <p class="tariff-two__info-item">+送礼物的能力</p>
+              </div>
+              <div class="btn btn-orange"><p>text</p></div>
+            </div>
+
+
+          </div>
+          <div class="tariff-three">
+            <div class="tariff-three__wrapper">
+              <p class="tariff-three__name">VIP 6 monts</p>
+              <div class="tariff-three__price">
+                <p>124.00Y</p>
+              </div>
+              <div class="tariff-three__info">
+                <p class="tariff-three__info-item">+一般聊天中的交流</p>
+                <p class="tariff-three__info-item">+使用相机</p>
+                <p class="tariff-three__info-item">+送礼物的能力</p>
+              </div>
+              <div class="btn btn-white"><p>text</p></div>
+            </div>
+
+
+          </div>
+        </div>
+      </div>
+    </section><!--tariffs-->
+
+
+    <section class="groups">
+      <div class="container">
+        <div class="groups-wrapper">
+          <div class="group-item">
+            <nuxt-link to="/group-profile">
+              <div class="group-item__top" style="background: url(/test_g_bg.png)"></div>
+              <div class="group-item__button">
+                <img draggable="false" src="/group_icon.png" alt="">
+              </div>
+              <div class="group-item__img">
+                <img draggable="false" src="https://www.wotcheatmods.com/wp-content/uploads/2020/04/440_wot_premium.png" alt="">
+              </div>
+              <div class="group-item__title">我们爱坦克世界</div>
+              <div class="group-item__subtitle">广播评级广播评级</div>
+              <div class="group-item__numbers">
+                <div class="group-item__number">
+                  <p>120</p>
+                  <p>广播的数</p>
+                </div>
+                <div class="group-item__number">
+                  <p>120</p>
+                  <p>广播的数</p>
+                </div>
+                <div class="group-item__number">
+                  <p>120</p>
+                  <p>广播的数</p>
+                </div>
+              </div>
+              <div class="group-item__avatars">
+              <span class="group-item__avatar">
+                  <img src="https://www.fillmurray.com/50/50" width="50" height="50"/>
+              </span>
+                <span class="group-item__avatar">
+                  <img src="https://www.fillmurray.com/100/100" width="50" height="50"/>
+              </span>
+                <span class="group-item__avatar">
+                <img src="https://www.fillmurray.com/200/200" width="50" height="50"/>
+              </span>
+                <span class="group-item__avatar">
+                <img src="https://www.fillmurray.com/150/150" width="50" height="50"/>
+              </span>
+                <span class="group-item__avatar">
+                  <img src="https://www.fillmurray.com/50/50" width="50" height="50"/>
+              </span>
+                <span class="group-item__avatar">
+                  <p>+ 12</p>
+              </span>
+              </div>
+              <div class="btn btn-blue-solid"><p>加入小组</p></div>
+            </nuxt-link>
+          </div>
+
+
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+  import SocialGroup from '@/components/SocialGroup';
+  // import Logo from '~/components/Logo.vue'
+  // import VuesaxLogo from '~/components/VuesaxLogo.vue'
+
+  export default {
+    components: {
+      //Logo,
+      //VuesaxLogo
+      SocialGroup
+    }
+  }
+</script>
+
+
